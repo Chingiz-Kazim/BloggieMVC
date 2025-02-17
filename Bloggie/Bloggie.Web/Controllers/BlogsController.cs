c211af2442119fe43f0ee5657c10b0a6deb8068e
Chingiz-Kazim <kazimov.chingiz.111@gmail.com>
2025-02-17 19:04:14 +0400

    adding like func

--- Bloggie/Bloggie.Web/Controllers/BlogsController.cs
+++ Bloggie/Bloggie.Web/Controllers/BlogsController.cs
@@ -1,4 +1,6 @@
-﻿using Bloggie.Web.Repositories;
+﻿using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers;
@@ -6,17 +8,40 @@ namespace Bloggie.Web.Controllers;
 public class BlogsController : Controller
 {
     private readonly IBlogPostRepository _blogPostRepository;
+    private readonly IBlogPostLikeRepository _blogPostLikeRepository;
 
-    public BlogsController(IBlogPostRepository blogPostRepository)
+    public BlogsController(IBlogPostRepository blogPostRepository, IBlogPostLikeRepository blogPostLikeRepository)
     {
         _blogPostRepository = blogPostRepository;
+        _blogPostLikeRepository = blogPostLikeRepository;
     }
 
     [HttpGet]
     public async Task<IActionResult> Index(string urlHandle)
     {
         var blogPost = await _blogPostRepository.GetByUrlHandleAsync(urlHandle);
+        var blogPostLikeVM = new BlogDetailsViewModel();
 
-        return View(blogPost);
+        if (blogPost != null)
+        {
+            var likes = await _blogPostLikeRepository.GetTotalLikes(blogPost.Id);
+
+            blogPostLikeVM = new BlogDetailsViewModel
+            {
+                Id = blogPost.Id,
+                Heading = blogPost.Heading,
+                PageTitle = blogPost.PageTitle,
+                Content = blogPost.Content,
+                Author = blogPost.Author,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                PublishDate = blogPost.PublishDate,
+                ShortDescription = blogPost.ShortDescription,
+                UrlHandle = urlHandle,
+                Visible = blogPost.Visible,
+                Tags = blogPost.Tags,
+                TotalLikes = likes,
+            };
+        }
+        return View(blogPostLikeVM);
     }
 }
--- Bloggie/Bloggie.Web/Models/ViewModels/BlogDetailsViewModel.cs
+++ Bloggie/Bloggie.Web/Models/ViewModels/BlogDetailsViewModel.cs
@@ -1,6 +1,8 @@
-﻿namespace Bloggie.Web.Models.Domain;
+﻿using Bloggie.Web.Models.Domain;
 
-public class BlogPost
+namespace Bloggie.Web.Models.ViewModels;
+
+public class BlogDetailsViewModel
 {
     public Guid Id { get; set; }
     public string Heading { get; set; }
@@ -14,5 +16,6 @@ public class BlogPost
     public bool Visible { get; set; }
 
     public ICollection<Tag> Tags { get; set; }
-    public ICollection<BlogPostLike> Likes { get; set; }
+
+    public int TotalLikes { get; set; }
 }
